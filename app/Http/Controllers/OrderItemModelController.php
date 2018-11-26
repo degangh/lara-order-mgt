8e38e65fa06bed81ee8fbd19cc3bb7637911dbf4
unknown <nicolash@fusion-solutions.com.au>
2018-11-26 17:02:22 +1030

    create OrderItem model and its associated migration/controller

---
+++ app/Http/Controllers/OrderItemModelController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\OrderItemModel;
+use Illuminate\Http\Request;
+
+class OrderItemModelController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\OrderItemModel  $orderItemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(OrderItemModel $orderItemModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\OrderItemModel  $orderItemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(OrderItemModel $orderItemModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\OrderItemModel  $orderItemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, OrderItemModel $orderItemModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\OrderItemModel  $orderItemModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(OrderItemModel $orderItemModel)
+    {
+        //
+    }
+}
---
+++ app/OrderItemModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItemModel extends Model
+{
+    //
+}
