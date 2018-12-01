5e374b8422436725b50db092a6f556e959d27286
nicolas <degang.huang@gmail.com>
2018-12-01 22:44:25 +1030

    add product relationship

--- app/OrderItem.php
+++ app/OrderItem.php
@@ -11,4 +11,9 @@ class OrderItem extends Model
     {
         return $this->belongsTo(Order::class);
     }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
