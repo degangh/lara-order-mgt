51483602f693dc918486f2d91732a4e8f617a710
nicolas <degang.huang@gmail.com>
2018-12-08 23:39:19 +1030

    modify test case to test auth api

--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -7,15 +7,19 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class RoutingTest extends TestCase
 {
+    
     /** @test */
     function get_order_list()
     {
+        $user = factory(\App\User::class)->create();
+        $this->actingAs($user, 'api');
+
         $this->json('get', '/api/orders')->assertStatus(200);
     }
 
     /** @test */
     function get_product_list()
-    {
+    {   
         $this->json('get', '/api/products')->assertStatus(200);
     }
 }
