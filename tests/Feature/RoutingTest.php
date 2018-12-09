119cd3177a04b02e2789108bb1eb412afc5a1eb5
nicolas <degang.huang@gmail.com>
2018-12-09 15:17:25 +1030

    add user authentication

--- tests/Feature/RoutingTest.php
+++ tests/Feature/RoutingTest.php
@@ -20,6 +20,22 @@ class RoutingTest extends TestCase
     /** @test */
     function get_product_list()
     {   
+        $user = factory(\App\User::class)->create();
+        $this->actingAs($user, 'api');
+
         $this->json('get', '/api/products')->assertStatus(200);
     }
+
+    /** @test */
+    function user_no_login_get_product_list()
+    {
+        $this->json('get', '/api/products')->assertStatus(401);
+    }
+
+    /** @test */
+    function user_no_login_get_order_list()
+    {
+        $this->json('get', '/api/orders')->assertStatus(401);
+    }
+
 }
