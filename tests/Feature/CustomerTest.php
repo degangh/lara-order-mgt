e8b8ae6f0ba97ab6a43f50ce9ab4d0dea72d92c4
nicolas <degang.huang@gmail.com>
2018-12-10 22:52:58 +1030

    boiler plate of customer test

---
+++ tests/Feature/CustomerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CustomerTest extends TestCase
+{
+    /** @test */
+    function a_login_user_can_create_customer()
+    {
+
+    }
+
+    /** @test */
+    function an_unauthenticated_user_cannot_create_customer()
+    {
+
+    }
+
+    /** @test */
+    function an_authenticated_user_can_get_customer_list()
+    {
+
+    }
+
+}
