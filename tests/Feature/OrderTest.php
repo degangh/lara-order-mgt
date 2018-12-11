e260bfea8c999602404040bf66f9cc9503ed0989
nicolas <degang.huang@gmail.com>
2018-12-11 22:17:40 +1030

    implement create order test

--- tests/Feature/OrderTest.php
+++ tests/Feature/OrderTest.php
@@ -8,12 +8,34 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class OrderTest extends TestCase
 {
+    
+    public function setup()
+    {
+        parent::setup();
+        $this->user = factory(\App\User::class)->create();
+        $this->customer = factory(\App\Customer::class)->create();
+    }
+    
     /** @test */
     public function an_authenticated_user_may_create_order()
     {
+        
         //given an authenticated user
-
+        $this->actingAs($this->user, 'api');
+        
         //when the user created order
+        $order = factory(\App\Order::class)->make([
+            'user_id' => $this->user->id,
+            'customer_id' => $this->customer->id
+            ]
+        );;
+        $this->json('post', '/api/order', $order->toArray())
+        ->assertStatus(200)
+        ->assertJsonStructure([
+            'user_id', 'customer_id', 'id', 'updated_at', 'created_at'
+            ]);
+
+
 
         //the order should be visible in the database
     }
