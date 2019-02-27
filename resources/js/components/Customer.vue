145c8a74eb10c43cf8dddb44fb1eb8cc14d2e701
unknown <degang.huang@gmail.com>
2019-02-27 14:52:31 +1030

    setup components

---
+++ resources/js/components/Customer.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+Customer page
+</div>
+</template>
---
+++ resources/js/components/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+        <div class="flex-center position-ref full-height">
+            <div class="content">
+                <div  class="m-b-md">
+                    <h2 class="title m-b-md">
+                        Welcome
+                    </h2>
+                    <h3>
+                    </h3>
+                </div>
+            </div>
+        </div>
+    </template>
+
+<script>
+export default {};
+</script>
+
+
+
