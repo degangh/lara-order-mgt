efc82e75fca0d5a49dbd2a276946a6b07bd3bfb1
nicolas <degang.huang@gmail.com>
2019-04-08 22:58:33 +0930

    add notFound component

---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <v-card>
+      <v-responsive :aspect-ratio="16/9">
+        <v-card-text>
+          The page you are looking for is not found
+        </v-card-text>
+      </v-responsive>
+    </v-card>
+  </div>
+</template>
