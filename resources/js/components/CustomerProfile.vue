5cbc63a2742118dfe1c42f45d57bc549a354f858
deganghnnmnnnnnnnn <degang.huang@gmail.com>
2019-05-10 16:54:05 +0930

    add reading customer data from api

--- resources/js/components/CustomerProfile.vue
+++ resources/js/components/CustomerProfile.vue
@@ -27,6 +27,7 @@
             </v-flex>
             
             <v-flex xs12>
+              <v-icon>notes</v-icon>
             </v-flex>
           </v-layout>
         </v-container>
@@ -37,6 +38,28 @@ export default {
 
     mounted() {
         console.log(this.$route.params.id)
+
+
+        let token = localStorage.getItem('jwt')
+
+        this.page = this.$route.params.page * 1
+
+        axios.defaults.headers.common['Content-Type'] = 'application/json'
+        axios.defaults.headers.common['Authorization'] = 'Bearer ' + token
+        this.requestCustomerInfo();
+
+    },
+
+    methods: {
+      requestCustomerInfo () {
+        axios.get('/api/customers/' + this.$route.params.id)
+          .then(this.handleResponse)
+      },
+
+      handleResponse (res) {
+        console.log(res)
+      }
+
     }
 
 }
