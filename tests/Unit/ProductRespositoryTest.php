03ce2ea3aad728e0bd07fcf7de3e588a2f35a215
deganghnnmnnnnnnnn <degang.huang@gmail.com>
2019-06-14 17:33:26 +0930

    redefine the test case

--- tests/Unit/ProductRespositoryTest.php
+++ tests/Unit/ProductRespositoryTest.php
@@ -26,8 +26,14 @@ class ProductRespositoryTest extends TestCase
         parent::tearDown();
     }
 
+    /** @test */
     public function it_can_save_product_info()
     {
+        //given a product name and price
+
+        //a product instance should be saved, and returned
+
+        //data base has the new created product information
 
     }
 }
