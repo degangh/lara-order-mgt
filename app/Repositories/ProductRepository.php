fe63123d8fb6f3e3dc63b41d717a3b981ea38238
nicolas <degang.huang@gmail.com>
2019-07-21 11:22:32 +0930

    Add keyword search to product dropdown

--- app/Repositories/ProductRepository.php
+++ app/Repositories/ProductRepository.php
@@ -6,9 +6,11 @@ use App\Product;
 
 class ProductRepository implements ProductRepositoryInterface
 {
-    public function all($records_per_page = 20)
+    public function all($keyword = '', $records_per_page = 20)
     {
-        return Product::paginate(20);
+        if (empty($keyword)) return Product::paginate(20);
+
+        return Product::where('name', 'like', '%' . $keyword . '%')->paginate(20);
     }
 
     public function create($attributes)
