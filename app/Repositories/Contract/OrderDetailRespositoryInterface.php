6adf900e0510367fb8ac78be33b4436f24baae6b
nicolas <degang.huang@gmail.com>
2019-09-21 11:41:11 +0930

    add repositories of the order

---
+++ app/Repositories/Contract/OrderDetailRespositoryInterface.php
@@ -0,0 +1,17 @@
+<?php
+namespace App\Repositories\Contract;
+
+interface OrderDetailRepositoryInterface
+{
+    /**
+     * get list of records, default 20 records
+     * 
+     * @parmas record_per_page default 20
+     * 
+     * @return 
+     */
+    public function all($record_per_page = 20);
+
+    public function create($items);
+
+}
---
+++ app/Repositories/OrderRepository.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Repositories;
+use App\Repositories\Contract\OrderRepositoryInterface;
+use App\Order;
+
+class OrderRepository implements OrderRepositoryInterface
+{
+
+}
