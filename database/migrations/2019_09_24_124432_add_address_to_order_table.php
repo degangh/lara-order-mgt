03d72cdd0afb811c511373887f1be4bb1d4dc299
nicolas <degang.huang@gmail.com>
2019-09-24 22:24:59 +0930

    add address to order table

---
+++ database/migrations/2019_09_24_124432_add_address_to_order_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAddressToOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->integer('address_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('address_id');
+        });
+    }
+}
