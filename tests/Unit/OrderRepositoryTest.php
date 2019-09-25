3aa1cc44293679965dc6771c8ad5c148d06132c8
nicolas <degang.huang@gmail.com>
2019-09-25 22:59:47 +0930

    add creating order unit test

--- tests/Unit/OrderRepositoryTest.php
+++ tests/Unit/OrderRepositoryTest.php
@@ -10,12 +10,16 @@ class OrderRepositoryTest extends TestCase
 {
     private $productRepository;
     private $order;
+    private $customer;
+    private $user;
+    private $address;
 
     public function setUp()
     {
         parent::setup();
-
-        $this->order = factory(\App\Order::class)->make();
+        $this->user = factory(\App\User::class)->create();
+        $this->customer = factory(\App\Customer::class)->create();
+        $this->address = factory(\App\Address::class)->create(['customer_id' => $this->customer->id]);
         $this->orderRepository = new \App\Repositories\OrderRepository;
     }
 
@@ -25,11 +29,20 @@ class OrderRepositoryTest extends TestCase
         parent::tearDown();
     }
 
+    /** @test */
     public function it_can_save_order_info()
     {
         //give customer and address
+        $this->order = factory(\App\Order::class)->make([
+            'customer_id' => $this->customer->id,
+            'address_id' => $this->address->id
+        ]);
+        var_dump($this->order);
 
+        $order = $this->orderRepository->create($this->order, $this->user);
         //an order instance should be save and returned
+        $this->assertInstanceOf('\App\Order', $order);
+        
 
         //the database should have new created order instance
 
