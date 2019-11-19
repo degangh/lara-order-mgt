2c9318385dbf50b6da4986c2a73f53a859396950
nicolas <degang.huang@gmail.com>
2019-11-19 16:37:24 +1030

    add associated customer

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -66,7 +66,7 @@ class OrderController extends Controller
      */
     public function show(Order $order)
     {
-        return Order::with('items', 'items.product')
+        return Order::with('customer','items', 'items.product')
         ->where('id',$order->id)->get();
     }
 
