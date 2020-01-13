1a622e2628e94e9ce6c2d8c9e6d0161493055dc1
deganghnnmnnnnnnnn <degang.huang@gmail.com>
2020-01-13 17:34:16 +1030

    rename interface

--- app/Repositories/OrderItemRepository.php
+++ app/Repositories/OrderItemRepository.php
@@ -1,11 +1,21 @@
 <?php
 
 namespace App\Repositories;
-use App\Repositories\Contract\OrderRepositoryInterface;
+use App\Repositories\Contract\OrderItemRepositoryInterface;
 use App\Order;
 use App\OrderItem;
 
 class OrderItemRepository implements OrderItemRepositoryInterface
 {
-
+    public function add(Order $order, OrderItem $item, $exchange_rate)
+    {
+        return OrderItem::create([
+            'order_id' => $order->id,
+            'product_id' => $item['id'],
+            'unit_price_cny' => $item['rrp_cny'],
+            'purchase_price_aud' => $item['ref_price_aud'],
+            'quantity' => $item['num'],
+            'exchange_rate' => $exchange_rate
+        ]);
+    }
 }
