e556a081cedc9e68b2860c6838d3f063ef1e5309
nicolas <degang.huang@gmail.com>
2020-01-25 23:37:09 +1030

    add new tests

--- tests/Unit/AddressRepositoryTest.php
+++ tests/Unit/AddressRepositoryTest.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class AddressRepositoryTest extends TestCase
 {
+    use WithFaker;
+    
     public function setup()
     {
         parent::setup();
@@ -24,5 +26,33 @@ class AddressRepositoryTest extends TestCase
         //a new address can be added 
         $address = $this->addressRepository->create($this->address);
         $this->assertInstanceOf('\App\Address', $address);
+    }
+
+    /** @test */
+    public function an_address_can_be_updated()
+    {
+        //given a customer and an existing address
+        $address = $this->addressRepository->create($this->address);
+        //and a set of updated address data
+        $new_mobile = $faker->phoneNumber;
+        $new_address = $faker->address;
+        $new_postcode = $faker->postcode;
+        $updated_address = factory(\App\Address::class)->create([
+            'id' => $address->id,
+            'mobile' => $new_mobile,
+            'address' => $new_address,
+            'postcode' => $postcode,
+            'customer_id' => $address->customer_id
+        ]);
+
+        $this->addressRepository->update($updated_product, $product);
+
+        $this->assertDatabaseHas('products', array(
+            'id' => $address->id,
+            'mobile' => $new_mobile,
+            'customer_id' => $address->customer_id,
+            'address' => $new_address,
+            'postcode' => $new_postcode
+        ));
     }
 }
