3aee539f03fdfa0c0b003fdc655995b488ea2812
nicolas <degang.huang@gmail.com>
2020-04-01 22:06:02 +1030

    optimize policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -14,7 +14,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
-        'App\OrderItem' => 'App\Policies\OrderItemPolicy'
+        'App\Order' => 'App\Policies\OrderPolicy'
     ];
 
     /**
