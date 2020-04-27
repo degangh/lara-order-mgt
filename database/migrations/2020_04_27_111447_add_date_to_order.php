4cb9d042051ea0c0d369106922c8d7f1b36b7816
nicolas <degang.huang@gmail.com>
2020-04-27 21:04:14 +0930

    add order date column to order table to record actual date of orders

---
+++ database/migrations/2020_04_27_111447_add_date_to_order.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddDateToOrder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->date('order_date')->default(date("Y-m-d"));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn('order_date');
+        });
+    }
+}
