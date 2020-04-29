26320e6a69e5d97f968a9c04f3d685dfcea99a45
nicolas <degang.huang@gmail.com>
2020-04-29 21:27:57 +0930

    add order_date to fillable

--- app/Order.php
+++ app/Order.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['user_id', 'customer_id', 'address_id'];
+    protected $fillable = ['user_id', 'customer_id', 'address_id', 'order_date'];
     
     //
     public function items()
